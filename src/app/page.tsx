d0a6a8f6bca1c48b0e1e4cefefaa55c768cfe473
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-06-24 18:26:59 +0900

    create temporary page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,6 @@ import { faBlog, faBook } from "@fortawesome/free-solid-svg-icons";
 
 import { SiQiita, SiZenn } from "@icons-pack/react-simple-icons";
 
-const navigation = [
-  { name: "Profile", href: "#" },
-  { name: "Works(under construction)", href: "#" },
-  { name: "Skill(under construction)", href: "#" },
-];
-
 const contacts = [
   {
     name: "GitHub",
@@ -65,21 +59,6 @@ const blogs = [
 export default function HomePage() {
   return (
     <>
-      <header className="hidden bg-sky-50 md:flex">
-        <nav className="mx-auto flex max-w-7xl items-center p-6 md:px-8">
-          <div className="hidden md:flex md:gap-x-14">
-            {navigation.map((item) => (
-              <a
-                className="text-sm font-semibold leading-6 text-gray-700"
-                key={item.name}
-                href={item.href}
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-        </nav>
-      </header>
       <main className="mx-auto max-w-3xl px-6 py-12">
         <div>
           <h1 className="text-4xl font-bold tracking-tight">
@@ -89,7 +68,7 @@ export default function HomePage() {
             Web developer / Software Engineer
           </p>
           {/* SNS Icons */}
-          <div className="flex flex-row justify-center gap-x-12">
+          <div className="mt-8 flex flex-row justify-center gap-x-12">
             {contacts.map((item) => (
               <a
                 key={item.name}
@@ -102,16 +81,20 @@ export default function HomePage() {
           </div>
           <div className="py-8">
             <h2 className="text-center text-2xl">Blog</h2>
-            <div className="mt-4 flex flex-row justify-center gap-x-12">
+            <div className="mt-4 flex flex-row justify-center gap-x-6">
               {blogs.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.link}
-                  rel="noopener noreferrer"
-                  className="hover:text-gray-500"
-                >
-                  {item.component}
-                </a>
+                <div className="h-20 w-40 items-center" key={item.name}>
+                  <a
+                    href={item.link}
+                    rel="noopener noreferrer"
+                    className="flex justify-center hover:text-gray-500"
+                  >
+                    {item.component}
+                  </a>
+                  <p className="hidden justify-center text-xs md:flex md:pt-2">
+                    {item.name}
+                  </p>
+                </div>
               ))}
             </div>
           </div>
