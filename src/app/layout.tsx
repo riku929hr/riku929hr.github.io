6a4634ecfadb74482a52a68b8745f9a619fce087
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-08-16 22:13:56 +0900

    add background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${NotoSansJPRegular.className} antialiased`}>
+    <html lang="jp" className={`${NotoSansJPRegular.className} antialiased`}>
       <body>{children}</body>
     </html>
   );
