4dc031bfd871893ab0997edc27e2d15f70f00b7e
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-08-22 23:35:04 +0900

    refactor and add About section

---
+++ src/app/_ui/toppage/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold tracking-tight">riku929hr</h1>
+      <h2 className="my-2 text-2xl font-bold">(Rikuto Sato)</h2>
+      <p className="my-4 text-lg leading-8">
+        Web developer / Software Engineer
+      </p>
+    </div>
+  );
+}
--- src/app/_ui/toppage/components/Section.tsx
+++ src/app/_ui/toppage/components/Section.tsx
@@ -5,7 +5,7 @@ type SectionProps = {
 const Section: React.FC<SectionProps> = ({ title, children }) => (
   <div className="py-4">
     <h2 className="text-2xl font-bold">{title}</h2>
-    {children}
+    <div className="py-4">{children}</div>
   </div>
 );
 
