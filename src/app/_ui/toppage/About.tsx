69995753bf9ce8ad293cef78d63672e874589118
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-08-23 23:31:19 +0900

    update about message

--- src/app/_ui/toppage/About.tsx
+++ src/app/_ui/toppage/About.tsx
@@ -2,7 +2,8 @@ import Section from "./components/Section";
 
 const About = () => (
   <Section title="About">
-    <p>erw</p>
+    <p>Webエンジニアです。</p>
+    <p>主にLaravel、React(TypeScript)を書いています。</p>
   </Section>
 );
 
